0f66e5633b6e6522b385368cd9aee1e71c4c4985
kennysoon <kennysoon0420@gmail.com>
2024-09-21 18:38:18 +0800

    run entity

--- src/Test.Test.EntityFrameworkCore/Migrations/20240920145410_Initial.Designer.cs
+++ src/Test.Test.EntityFrameworkCore/Migrations/20240920145410_Initial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Test.Test.EntityFrameworkCore;
 
@@ -11,9 +12,11 @@ using Test.Test.EntityFrameworkCore;
 namespace Test.Test.Migrations
 {
     [DbContext(typeof(TestDbContext))]
-    partial class TestDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240920145410_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
